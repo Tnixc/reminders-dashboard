0e0eebe7721b4803ae6ac141384a2eb97b6e8542
Tnixc <tnixxc@gmail.com>
2025-10-23 11:09:06 -0400

    ok

--- main.go
+++ main.go
@@ -8,16 +8,16 @@ import (
 
 	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
+	boxer "github.com/treilik/bubbleboxer"
 )
 
-type errMsg error
+const (
+	leftAddr  = "left"
+	rightAddr = "right"
+)
 
 type model struct {
-	list     listModel
-	quitting bool
-	err      error
-	width    int
-	height   int
+	tui boxer.Boxer
 }
 
 var quitKeys = key.NewBinding(
@@ -25,53 +25,106 @@ var quitKeys = key.NewBinding(
 	key.WithHelp("", "press q to quit"),
 )
 
+func stripErr(n boxer.Node, _ error) boxer.Node {
+	return n
+}
+
 func initialModel() model {
-	return model{
-		list: newListModel(),
+	// Create models
+	left := listModelHolder{m: newListModel()}
+	right := stringer("Hello World")
+
+	// Layout tree definition
+	m := model{tui: boxer.Boxer{}}
+	m.tui.LayoutTree = boxer.Node{
+		VerticalStacked: false, // horizontal split
+		SizeFunc: func(_ boxer.Node, widthOrHeight int) []int {
+			// Split width 66/33
+			ratio := widthOrHeight / 3
+			return []int{2 * ratio, widthOrHeight - ratio}
+		},
+		Children: []boxer.Node{
+			stripErr(m.tui.CreateLeaf(leftAddr, left)),
+			stripErr(m.tui.CreateLeaf(rightAddr, right)),
+		},
 	}
+
+	return m
 }
 
 func (m model) Init() tea.Cmd {
-	return m.list.Init()
+	return nil
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-
 	case tea.KeyMsg:
 		if key.Matches(msg, quitKeys) {
-			m.quitting = true
 			return m, tea.Quit
 		}
-
 	case tea.WindowSizeMsg:
-		m.width = msg.Width
-		m.height = msg.Height
-
-	case errMsg:
-		m.err = msg
-		return m, nil
+		m.tui.UpdateSize(msg)
 	}
 
-	// Pass message to list
+	// Update the list model
 	var cmd tea.Cmd
-	updatedList, cmd := m.list.Update(msg)
-	m.list = updatedList.(listModel)
+	m.editModel(leftAddr, func(v tea.Model) (tea.Model, error) {
+		v, cmd = v.Update(msg)
+		return v, nil
+	})
+
 	return m, cmd
 }
 
 func (m model) View() string {
-	if m.err != nil {
-		return m.err.Error()
-	}
-
-	// Don't render until we have dimensions
-	if m.width == 0 || m.height == 0 {
-		return ""
-	}
-
-	return m.list.View()
+	return m.tui.View()
 }
+
+func (m *model) editModel(addr string, edit func(tea.Model) (tea.Model, error)) error {
+	if edit == nil {
+		return fmt.Errorf("no edit function provided")
+	}
+	v, ok := m.tui.ModelMap[addr]
+	if !ok {
+		return fmt.Errorf("no model with address '%s' found", addr)
+	}
+	v, err := edit(v)
+	if err != nil {
+		return err
+	}
+	m.tui.ModelMap[addr] = v
+	return nil
+}
+
+// listModelHolder wraps listModel to satisfy tea.Model
+type listModelHolder struct {
+	m listModel
+}
+
+func (l listModelHolder) Init() tea.Cmd {
+	return l.m.Init()
+}
+
+func (l listModelHolder) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	m, cmd := l.m.Update(msg)
+	l.m = m.(listModel)
+	return l, cmd
+}
+
+func (l listModelHolder) View() string {
+	return l.m.View()
+}
+
+// stringer is a simple string model
+type stringer string
+
+func (s stringer) String() string {
+	return string(s)
+}
+
+func (s stringer) Init() tea.Cmd                           { return nil }
+func (s stringer) Update(msg tea.Msg) (tea.Model, tea.Cmd) { return s, nil }
+func (s stringer) View() string                            { return s.String() }
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
